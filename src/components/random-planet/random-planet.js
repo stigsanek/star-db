c2dcd196a303a394f9f0225d7bf89d400c9a13ce
stigsanek <stigsanek@yandex.ru>
2020-08-03 16:26:46 +0400

    Переносит логику трансформации данных в SwapiService

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -7,11 +7,7 @@ export default class RandomPlanet extends Component {
   swapiService = new SwapiService();
 
   state = {
-    id: null,
-    name: null,
-    population: null,
-    rotationPeriod: null,
-    diameter: null
+    planet: {}
   }
 
   constructor() {
@@ -19,22 +15,21 @@ export default class RandomPlanet extends Component {
     this.updatePlanet();
   }
 
-  updatePlanet() {
-    const id = Math.floor((Math.random() * 25) + 1);
+  generateId() {
+    return Math.floor((Math.random() * 15) + 2);
+  }
 
-    this.swapiService.getPlanet(id).then((planet) => {
-      this.setState({
-        id,
-        name: planet.name,
-        population: planet.population,
-        rotationPeriod: planet.rotation_period,
-        diameter: planet.diameter
-      })
-    });
+  onPlanetLoaded = (planet) => {
+    this.setState({ planet });
+  }
+
+  updatePlanet() {
+    const id = this.generateId();
+    this.swapiService.getPlanet(id).then(this.onPlanetLoaded);
   }
 
   render() {
-    const { id, name, population, rotationPeriod, diameter } = this.state;
+    const { planet: { id, name, population, rotationPeriod, diameter } } = this.state;
 
     return (
       <div className="random-planet jumbotron rounded">
